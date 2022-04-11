56c53ec4b76c965e8ac7a8615eb08eac2521ecfc
Walter <alber13467@gmail.com>
2022-04-11 10:49:43 +0800

    4

--- network_w8_p1/Form1.Designer.cs
+++ network_w8_p1/Form1.Designer.cs
@@ -44,52 +44,52 @@ namespace network_w8_p1
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.Location = new System.Drawing.Point(59, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 26);
+            this.label1.Size = new System.Drawing.Size(73, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "目標IP";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(390, 58);
+            this.label2.Location = new System.Drawing.Point(462, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 26);
+            this.label2.Size = new System.Drawing.Size(97, 26);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "目標Port";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(718, 58);
+            this.label3.Location = new System.Drawing.Point(776, 58);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 26);
+            this.label3.Size = new System.Drawing.Size(97, 26);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "接聽Port";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.textBox1.Location = new System.Drawing.Point(160, 58);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(170, 33);
+            this.textBox1.Size = new System.Drawing.Size(257, 33);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox2.Location = new System.Drawing.Point(489, 56);
+            this.textBox2.Location = new System.Drawing.Point(608, 56);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(170, 33);
+            this.textBox2.Size = new System.Drawing.Size(127, 33);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox3.Location = new System.Drawing.Point(867, 56);
+            this.textBox3.Location = new System.Drawing.Point(916, 56);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(170, 33);
+            this.textBox3.Size = new System.Drawing.Size(121, 33);
             this.textBox3.TabIndex = 5;
             // 
             // button1
@@ -99,7 +99,7 @@ namespace network_w8_p1
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 33);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "傳送";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -107,7 +107,7 @@ namespace network_w8_p1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1258, 145);
+            this.ClientSize = new System.Drawing.Size(1258, 570);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -121,6 +121,7 @@ namespace network_w8_p1
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- network_w8_p1/Form1.cs
+++ network_w8_p1/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -29,7 +30,32 @@ namespace network_w8_p1
 
         private void Listen()
         {
+            int Port = int.Parse(textBox3.Text);
+            U = new UdpClient(Port);
 
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);
+                string A = Encoding.Default.GetString(B);
+                string[] Q = A.Split('/');
+                Point[] R = new Point[Q.Length];
+                for(int i = 0; i< Q.Length; i++)
+                {
+                    string[] k = Q[i].Split(',');
+                    R[i].X = int.Parse(k[0]);
+                    R[i].Y = int.Parse(k[1]);
+                }
+
+                for(int i = 0;i<Q.Length - 1; i++)
+                {
+                    LineShape L = new LineShape();
+                    L.StartPoint = R[i];
+                    L.EndPoint = R[i + 1];
+                    L.Parent = D;
+                }
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -79,6 +105,15 @@ namespace network_w8_p1
 
                 p += "/" + stP.X.ToString() + "," + stP.Y.ToString(); //持續記錄座標
             }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(textBox2.Text);
+            UdpClient S = new UdpClient(textBox1.Text, Port);
+            byte[] B = Encoding.Default.GetBytes(p);
+            S.Send(B, B.Length);
+            S.Close();
         }
     }
 }
